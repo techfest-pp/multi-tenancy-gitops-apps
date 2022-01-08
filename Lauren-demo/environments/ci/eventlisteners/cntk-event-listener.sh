cc6728efba7d99ab24e6062f6577bc55e66be1f4
gshrestha <Gunu.Shrestha@ibm.com>
2022-01-07 20:08:08 -0800

    kustomization and cntk event listener corrected

--- Lauren-demo/environments/ci/eventlisteners/cntk-event-listener.sh
+++ Lauren-demo/environments/ci/eventlisteners/cntk-event-listener.sh
@@ -6,10 +6,12 @@ if [[ -z ${GIT_ORG} ]]; then
   exit 1
 fi
 
-GIT_BRANCH=${GIT_BRANCH_Lauren-demo:-master}
+GIT_BRANCH_QM1=${GIT_BRANCH_QM1:-master}
+GIT_BRANCH=${GIT_BRANCH_LAUREN-DEMO:-master}
 
 # Create Kubernetes Secret yaml
 ( echo "cat <<EOF" ; cat cntk-event-listener.yaml_template ; echo EOF ) | \
 GIT_ORG=${GIT_ORG} \
-GIT_BRANCH=${GIT_BRANCH_Lauren-demo:-master} \
+GIT_BRANCH_QM1=${GIT_BRANCH_QM1:-master} \
+GIT_BRANCH=${GIT_BRANCH_LAUREN-DEMO:-master} \
 sh > cntk-event-listener.yaml
